cf1fa55d4e92d8dc6b3fc9ca745e33d9fce2f0d0
Renata21 <celac.renata@gmail.com>
2023-03-21 09:54:14 +0200

    fix

--- src/magazin_animale/custom_dialog/AddEditDialogView.java
+++ src/magazin_animale/custom_dialog/AddEditDialogView.java
@@ -3,10 +3,12 @@ package magazin_animale.custom_dialog;
 import javax.swing.*;
 import java.awt.*;
 
+import static magazin_animale.database.DBDataAnimale.getIDByName;
+
 public class AddEditDialogView {
     static JTextField specie = new JTextField(10);
     static JTextField rasa = new JTextField(10);
-    static JSpinner pret = new JSpinner(new SpinnerNumberModel(0, 0, 100000, 1));
+    static JSpinner pret = new JSpinner(new SpinnerNumberModel(0, 0, 100000, 100));
     static JSpinner numar = new JSpinner(new SpinnerNumberModel(0, 0, 1000, 1));
 
 
@@ -46,11 +48,21 @@ public class AddEditDialogView {
         int result = JOptionPane.showConfirmDialog(parentComponent, principalPanel, option, JOptionPane.OK_CANCEL_OPTION);
 
         if (result == JOptionPane.OK_OPTION) {
+            if (specie.getText().length() == 0 || rasa.getText().length() == 0) {
+                JOptionPane.showMessageDialog(parentComponent, "Numele sau rasa nu pot fi goale", "Eroare de validare", JOptionPane.ERROR_MESSAGE);
+                return null;
+            }
+
+            if(getIDByName(specie.getText() + "_" + rasa.getText()) != -1 && option.equals("Adauga animal")){
+                JOptionPane.showMessageDialog(parentComponent, "Exista deja un astfel de animal.", "Eroare de validare", JOptionPane.ERROR_MESSAGE);
+                return null;
+            }
             input[0] = specie.getText();
             input[1] = rasa.getText();
             input[2] = pret.getValue().toString();
             input[3] = specie.getText() + "_" + rasa.getText();
             input[4] = numar.getValue().toString();
+
         } else input = null;
 
         return input;
